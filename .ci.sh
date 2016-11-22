e876662102adc3405e17cbee65b072c4bab796ba
Maldivi Christophe <christophe.maldivi@orange.com>
2016-11-23 00:01:40 +0100

    chore(travis): debug travis

--- .ci.sh
+++ .ci.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-(cd simdoc/core/java/libs && ./mvnw clean install)
+(cd simdoc/core/java/libs && ./mvnw clean install) && \
+(cd simdoc/apps/pc/ && ./compileAll.sh) && \
+(cd simdoc/apps/pc/ && ./refIntegrationTest.sh) && \
+echo OK
