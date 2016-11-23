a525350ccf80801a0d601b002d3ab4be907b41d2
Maldivi Christophe <christophe.maldivi@orange.com>
2016-11-23 22:38:09 +0100

    chore(travis): deploy to github

--- .ci.sh
+++ .ci.sh
@@ -13,6 +13,6 @@ cp simdoc/apps/pc/Graph/build/libs/*.jar Graph-$TAG.jar && \
 cp simdoc/apps/pc/Multisets/build/libs/*.jar Multisets-$TAG.jar && \
 cp simdoc/apps/pc/Ocr/build/libs/*.jar Ocr-$TAG.jar && \
 echo Create tarball && \
-tar cvjf documentare-simdoc.tar.bz2 *.jar
+tar cvjf documentare-simdoc-$TAG.tar.bz2 *.jar
 echo && \
 echo OK
