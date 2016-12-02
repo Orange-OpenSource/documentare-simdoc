e56757646180b91c846eef6f12c46e30caca059d
Maldivi Christophe <christophe.maldivi@orange.com>
2016-12-02 20:18:30 +0100

    chore(ref test): update PrepClustering option

--- simdoc/apps/pc/doImgClusteringOn.sh
+++ simdoc/apps/pc/doImgClusteringOn.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ABS_IMG=`pwd`/$1
-(cd LineDetection/ && ./go.sh $ABS_IMG && rm -f ld_out/*.raw) && (cd Ncd && ./go.sh -file1 ../LineDetection/ld_out) && (cd SimClustering/ && ./go.sh -i ../Ncd/ncd_nearests_ready_for_simclustering.json.gz -scut -ccut) && (cd Graph && ./go.sh -i ../SimClustering/sc_graph_input.json.gz -d ../Ncd/thumbnails && ./show.sh)
+(cd LineDetection/ && ./go.sh $ABS_IMG && rm -f ld_out/*.raw) && (cd Ncd && ./go.sh -file1 ../LineDetection/ld_out) && (cd PrepClustering && ./go.sh -f ../Ncd/ncd_regular_files_model.json.gz) && (cd SimClustering/ && ./go.sh -i ../PrepClustering/prep_clustering_ready.json.gz -scut -ccut) && (cd Graph && ./go.sh -i ../SimClustering/sc_graph_input.json.gz -d ../Ncd/thumbnails && ./show.sh)
