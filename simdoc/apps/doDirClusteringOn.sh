78a3510d05d2ae795346e00bc800176a34f4dd40
Maldivi Christophe <christophe.maldivi@orange.com>
2016-12-16 08:50:15 +0100

    chore(script): update script

--- simdoc/apps/doDirClusteringOn.sh
+++ simdoc/apps/doDirClusteringOn.sh
@@ -2,6 +2,6 @@
 
 ABS_DIR=`pwd`/$1
 (cd Ncd && ./go.sh -d1 $ABS_DIR) && \
-(cd PrepClustering && ./go.sh -f ../Ncd/ncd_regular_files_model.json.gz) && \
-(cd SimClustering/ && ./go.sh -i ../PrepClustering/prep_clustering_ready.json.gz -scut -ccut) && \
-(cd Graph && ./go.sh -i ../SimClustering/sc_graph_input.json.gz -d ../Ncd/thumbnails && ./show.sh)
+(cd PrepClustering && ./go.sh -json ../Ncd/ncd_regular_files_model.json.gz) && \
+(cd SimClustering/ && ./go.sh -json ../PrepClustering/prep_clustering_ready.json.gz -acut -qcut -scut -ccut) && \
+(cd Graph && ./go.sh -json ../SimClustering/sc_graph_input.json.gz -d ../Ncd/thumbnails && ./show.sh)
