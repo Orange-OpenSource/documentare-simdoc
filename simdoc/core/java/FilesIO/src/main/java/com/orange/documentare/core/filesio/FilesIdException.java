66c52de44c2bf08fe2deb94595de64b1b412c9c0
Christophe Maldivi <christophe.maldivi@orange.com>
2016-12-20 14:20:51 +0100

    feat(FilesIO): add files id builder

---
+++ simdoc/core/java/FilesIO/src/main/java/com/orange/documentare/core/filesio/FilesIdException.java
@@ -0,0 +1,7 @@
+package com.orange.documentare.core.filesio;
+
+public class FilesIdException extends RuntimeException {
+  public FilesIdException(String msg) {
+    super(msg);
+  }
+}
---
+++ simdoc/core/java/FilesIO/src/main/java/com/orange/documentare/core/filesio/FilesIdMap.java
@@ -0,0 +1,6 @@
+package com.orange.documentare.core.filesio;
+
+import java.util.HashMap;
+
+public class FilesIdMap extends HashMap<Integer, String> {
+}
