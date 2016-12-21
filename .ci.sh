c3c460147980201370832b9eb53448640cbef56f
Christophe Maldivi <christophe.maldivi@orange.com>
2016-12-21 10:10:43 +0100

    chore(travis): add PrepInputDir as jar artifac

--- .ci.sh
+++ .ci.sh
@@ -5,6 +5,7 @@ TAG=`git describe`
 (cd simdoc && ./prep-simdoc.sh) && \
 (cd simdoc/apps && ./refIntegrationTest.sh) && \
 cp simdoc/apps/LineDetection/target/Line*.jar LineDetection-$TAG.jar && \
+cp simdoc/apps/PrepInputDir/target/Prep*.jar PrepInputDir-$TAG.jar && \
 cp simdoc/apps/Ncd/target/Ncd*.jar Ncd-$TAG.jar && \
 cp simdoc/apps/PrepClustering/target/Prep*.jar PrepClustering-$TAG.jar && \
 cp simdoc/apps/SimClustering/target/Sim*.jar SimClustering-$TAG.jar && \
