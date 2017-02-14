41a85c338372ad9838072f9ddbc1103125c1b2d8
Maldivi Christophe <christophe.maldivi@orange.com>
2017-02-14 09:49:57 +0100

    chore(renaming): start renaming apps to lowercase

--- .ci.sh
+++ .ci.sh
@@ -2,18 +2,18 @@
 
 TAG=`git describe`
 
-(cd simdoc && ./prep-simdoc.sh) && \
-(cd simdoc/SimdocServer && ./mvnw clean install) && \
-(cd simdoc/apps && ./refIntegrationTest.sh) && \
-cp simdoc/SimdocServer/target/SimdocServer-1.0.0-SNAPSHOT.jar SimdocServer-$TAG.jar && \
-cp simdoc/apps/LineDetection/target/Line*.jar LineDetection-$TAG.jar && \
-cp simdoc/apps/PrepInputDir/target/Prep*.jar PrepInputDir-$TAG.jar && \
-cp simdoc/apps/Ncd/target/Ncd*.jar Ncd-$TAG.jar && \
-cp simdoc/apps/PrepClustering/target/Prep*.jar PrepClustering-$TAG.jar && \
-cp simdoc/apps/SimClustering/target/Sim*.jar SimClustering-$TAG.jar && \
-cp simdoc/apps/Thumbnails/target/Thumbnails*.jar Thumbnails-$TAG.jar && \
-cp simdoc/apps/Graph/target/Graph*.jar Graph-$TAG.jar && \
-cp simdoc/SimdocServer/target/SimdocServer*.jar SimdocServer-$TAG.jar && \
+#(cd simdoc && ./prep-simdoc.sh) && \
+#(cd simdoc/simdoc-server && ./mvnw clean install) && \
+#(cd simdoc/apps && ./refIntegrationTest.sh) && \
+cp simdoc/simdoc-server/target/simdoc-server-1.0.0-SNAPSHOT.jar simdoc-server-$TAG.jar && \
+cp simdoc/apps/line-detection/target/line*.jar line-detection-$TAG.jar && \
+cp simdoc/apps/prep-data/target/prep*.jar prep-data-$TAG.jar && \
+cp simdoc/apps/ncd/target/ncd*.jar ncd-$TAG.jar && \
+cp simdoc/apps/prep-clustering/target/prep*.jar prep-clustering-$TAG.jar && \
+cp simdoc/apps/similarity-clustering/target/sim*.jar similarity-clustering-$TAG.jar && \
+cp simdoc/apps/thumbnails/target/thumbnails*.jar thumbnails-$TAG.jar && \
+cp simdoc/apps/graph/target/graph*.jar graph-$TAG.jar && \
+cp simdoc/simdoc-server/target/simdoc-server*.jar simdoc-server-$TAG.jar && \
 echo Create tarball && \
 tar cvjf documentare-simdoc-$TAG.tar.bz2 *.jar && \
 echo && \
