3b90916811e3f8722776b57ff264b1a4d5a396d9
Denis Boisset <denis.boisset@orange.com>
2017-03-06 16:32:42 +0100

    fix(matrix csv): avoid locale issue (float number) by specifying Locale.US

--- simdoc/core/java/Comp/src/main/java/com/orange/documentare/core/comp/distance/matrix/DistancesMatrixCsvGzipWriter.java
+++ simdoc/core/java/Comp/src/main/java/com/orange/documentare/core/comp/distance/matrix/DistancesMatrixCsvGzipWriter.java
@@ -20,10 +20,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.zip.GZIPOutputStream;
 
 @Slf4j
@@ -144,11 +141,11 @@ public class DistancesMatrixCsvGzipWriter {
   }
 
   private void showMatrixCell(OutputStream outputStream, int distance) {
-    writeStringTo(outputStream, String.format("%f%s", getOutputDistanceFormat(distance), MATRIX_SEP));
+    writeStringTo(outputStream, String.format(Locale.US, "%f%s", getOutputDistanceFormat(distance), MATRIX_SEP));
   }
 
   private void showNearestsCell(OutputStream outputStream, String nearestName, int distance) {
-    writeStringTo(outputStream, String.format("%s %f%s", nearestName, getOutputDistanceFormat(distance), MATRIX_SEP));
+    writeStringTo(outputStream, String.format(Locale.US, "%s %f%s", nearestName, getOutputDistanceFormat(distance), MATRIX_SEP));
   }
 
   private double getOutputDistanceFormat(int distance) {
