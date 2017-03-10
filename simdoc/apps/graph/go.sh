e58dfe1c4db1f6af35cc7496964bda6df0a52518
Maldivi Christophe <christophe.maldivi@orange.com>
2017-03-10 16:21:19 +0100

    feat(thumbnails): add possibility to use a different directory to build thumbnails

--- simdoc/apps/graph/go.sh
+++ simdoc/apps/graph/go.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rm -rf out *.csv
+rm -rf out *.csv source-symlinks thumbnails
 java -jar target/graph-1.0-SNAPSHOT.jar $1 $2 $3 $4 $5 $6
--- simdoc/apps/graph/src/main/java/com/orange/documentare/app/graph/ThumbnailsBuilder.java
+++ simdoc/apps/graph/src/main/java/com/orange/documentare/app/graph/ThumbnailsBuilder.java
@@ -18,6 +18,7 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Map;
 import java.util.Optional;
 
@@ -28,9 +29,11 @@ public class ThumbnailsBuilder {
 
   private final SymbolicLinkConverter symbolicLinkConverter = new SymbolicLinkConverter();
   private final Metadata metadata;
+  private final Optional<File> thumbnailsSourceDirectory;
+  private final String inputDirectoryPath;
 
   /** @return optional image directory absolute path */
-  public Optional<String> build() {
+  public Optional<File> build() {
     FileUtils.deleteQuietly(SOURCE_SYMLINKS_DIR);
     FileUtils.deleteQuietly(THUMBNAILS_DIR);
     SOURCE_SYMLINKS_DIR.mkdir();
@@ -39,7 +42,7 @@ public class ThumbnailsBuilder {
     metadata.filesMap.entrySet().parallelStream()
       .forEach(this::buildThumbnail);
 
-    return Optional.of(THUMBNAILS_DIR.getAbsolutePath());
+    return Optional.of(THUMBNAILS_DIR);
   }
 
   private void buildThumbnail(Map.Entry<Integer, String> mapEntry) {
@@ -51,14 +54,38 @@ public class ThumbnailsBuilder {
         Thumbnail.createThumbnail(sourceImage, destImage, label);
       }
     } catch (IOException e) {
-      throw new IllegalStateException(String.format("Failed to create thumbnail '%s' from file '%s': %s", sourceImage.getAbsolutePath(), destImage.getAbsolutePath(), e.getMessage()));
+      throw new IllegalStateException(String.format("Failed to create thumbnail '%s' from file '%s': %s", sourceImage.getAbsolutePath(), destImage.getAbsolutePath()));
     }
   }
 
   private File buildSymLink(Map.Entry<Integer, String> mapEntry) {
-    File source = new File(mapEntry.getValue());
+    String sourceAbsPath = buildSourceAbsPath(mapEntry.getValue());
+    File source = new File(sourceAbsPath);
     File destination = new File(SOURCE_SYMLINKS_DIR.getAbsolutePath() + File.separator + mapEntry.getKey());
     symbolicLinkConverter.convert(source, destination);
     return destination;
+  }
+
+  private String buildSourceAbsPath(String filePath) {
+    return thumbnailsSourceDirectory.isPresent() ? buildPathFromThumbnailSourceDir(filePath) : filePath;
+  }
+
+  private String buildPathFromThumbnailSourceDir(String filePath) {
+    String sourceAbsPath;
+    String relativePath = filePath.replace(inputDirectoryPath, "");
+    if (relativePath.contains(".")) {
+      int extensionIndex = relativePath.lastIndexOf(".");
+      relativePath = relativePath.substring(0, extensionIndex);
+    }
+    Collection<File> files = FileUtils.listFiles(thumbnailsSourceDirectory.get(), null, true);
+    final String searchPath = relativePath;
+    Optional<File> sourceFile = files.stream()
+      .filter(file -> file.getAbsolutePath().contains(searchPath))
+      .findFirst();
+    if (!sourceFile.isPresent()) {
+      throw new IllegalStateException(String.format("Failed to find thumbnail source file in directory '%s', for file %s", thumbnailsSourceDirectory.get().getAbsolutePath(), filePath));
+    }
+    sourceAbsPath = sourceFile.get().getAbsolutePath();
+    return sourceAbsPath;
   }
 }
