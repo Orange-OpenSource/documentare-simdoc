a23a291e5c19b77dd680d6355645a04bbd6c472e
Christophe Maldivi <christophe.maldivi@orange.com>
2017-06-30 22:15:52 +0200

    chore(debian): add dh_make debian template creation file

---
+++ debian/simdoc-server.default.ex
@@ -0,0 +1,10 @@
+# Defaults for simdoc-server initscript
+# sourced by /etc/init.d/simdoc-server
+# installed at /etc/default/simdoc-server by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
