57b659d4e1f4da985977b8756fece49af7a7f366
Christophe Maldivi <christophe.maldivi@orange.com>
2017-07-07 20:36:25 +0200

    fix(release): tools jar are added to the delivery

--- .ci.sh
+++ .ci.sh
@@ -8,7 +8,7 @@ echo git tag is: $TAG
 
 make build-tests integration-test deb && \
 echo Create tar && \
-tar cvf documentare-simdoc-$TAG.tar usr ../simdoc*.deb && \
+tar cvf documentare-simdoc-$TAG.tar *.jar usr ../simdoc*.deb && \
 echo && \
 echo OK && exit 0
 
